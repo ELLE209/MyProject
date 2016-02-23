97c8b36709c6f4b6bc1f548342d3a84aed1b69c7
ELLE209 <polinalang1@hotmail.com>
2016-02-24 00:01:55 +0200

    DB initialization

--- MyProject/flaskr/flaskr.py
+++ MyProject/flaskr/flaskr.py
@@ -8,15 +8,46 @@ My Project...
 import sqlite3
 from flask import Flask, request, g, redirect, url_for, \
     abort, render_template, flash
+from contextlib import closing
 # endregion
 
 # region ------------------ CONFIGURATIONS -----------------------
-DATABASE = '/tmp/flaskr.db'
+#DATABASE = 'C:\Users\Elizabeth\MyDB.db'
+DATABASE = 'C:\Users\Elizabeth\Documents\GitHub\MyProject\MyProject\databases\MyDB.db'
 DEBUG = True
 SECRET_KEY = 'development key'
 USERNAME = 'admin'
 PASSWORD = 'default'
 # endregion
 
-app = Flask(__name__)
-app.config.from_object(__name__)
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+
+def init_db0():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+def init_db():
+    with closing(connect_db()) as db:
+        cursor = db.cursor().execute("select ID, username, password from users where username = 'elle' and spID = 1")
+
+        for row in cursor.fetchall():
+            id, username, password = row
+            print ' {%d}: %s, %s' % (id, username, password)
+
+def init_db2():
+    db = closing(connect_db())
+    f = app.open_resource('schema.sql', mode='r')
+
+    db.cursor().executescript(f.read())
+    db.commit()
+
+if __name__ == '__main__':
+    app = Flask(__name__)
+    app.config.from_object(__name__)
+    init_db()
+    app.run()
--- MyProject/flaskr/schema.sql
+++ MyProject/flaskr/schema.sql
@@ -4,3 +4,14 @@ create table entries (
   title text not null,
   text text not null
 );
+
+sqlite> CREATE TABLE main.users(
+ID integer primary key autoincrement,
+username text not null,
+password text not null,
+spID int not null,
+spUserID int not null
+);
+
+INSERT INTO main.users (ID, username, password, spID, spUserID) 
+VALUES (1, 'elle', '12345', 1, 11);

12c46c0ee5cabeb2a0256054ddbbc420c4151254
ELLE209 <34v7@34v7-13.tichon.guttman>
2016-02-28 10:33:40 +0200

    update 28.2
    Added user verification according to db in MyMainServer.

--- MyProject/DataBaseManager.py
+++ MyProject/DataBaseManager.py
@@ -19,37 +19,38 @@ class DataBaseManager(object):
         query += ")"
         self.cur.execute(query)
 
-    def  exec_query(self, query):
+    def exec_query(self, query):
         self.cur.execute(query)
+        return self.cur.fetchone()
 
-    def  insert(self, table_name, data, fields):
+    def insert(self, table_name, fields):
         query = "INSERT INTO " + table_name + " VALUES("
-        for i in xrange(len(fields)):
+        for i in xrange(len(fields[0])):
             query += '?,'
         query = query[:-1]
         query += ")"
-        self.cur.executemany(query, data)
+        self.cur.executemany(query, fields)
 
-    def  get_rows(self, table_name):
+    def get_rows(self, table_name):
         self.cur.execute("SELECT * FROM " + table_name)
         rows = self.cur.fetchall()
-        return  rows
+        return rows
 
-    def  printTable(self, table_name):
+    def print_table(self, table_name):
         rows = self.get_rows(table_name)
         for row in rows:
             print row
 
     def get_dict_from_table(self,table_name):
-        ''' Turn a list with sqlite3.Row objects into a dictionary'''
-        dict ={} # the dictionary to be filled with the row data and to be returned
+        # Turn a list with sqlite3.Row objects into a dictionary
+        dictionary = {}  # the dictionary to be filled with the row data and to be returned
 
         for i, row in enumerate(self.get_rows(table_name)): # iterate throw the sqlite3.Row objects
             fields = [] # for each Row use a separate list
             for col in range(0, len(row)): # copy over the row date (ie. column data) to a list
                 fields.append(row[col])
-            dict[fields[1]] = (fields[2], fields[3]) # add the list to the dictionary
-        return dict
+            dictionary[fields[1]] = (fields[2], fields[3]) # add the list to the dictionary
+        return dictionary
 
     def set_dict_to_table(self,table_name,dict):
         keys = ','.join(dict.keys())
@@ -57,5 +58,5 @@ class DataBaseManager(object):
         for word in dict.keys():
             value = dict[word]
             self.cur.execute("INSERT INTO "+ table_name +" VALUES (" + str(i) + ",'" + word + "'," + str(value[0]) + "," + str(value[1]) + ")" )
-            i+=1
+            i += 1
         self.con.commit()
--- MyProject/MyMainServerWEB.py
+++ MyProject/MyMainServerWEB.py
@@ -26,9 +26,21 @@ data_base_manager = DataBaseManager()
 # endregion
 
 
-@app.route('/login')
+# route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return 'Hello, welcome!'
+    error = None
+    if request.method == 'POST':
+        # find user in database
+        username = request.form['username']
+        password = request.form['password']
+        query = "SELECT spUserID FROM Users WHERE username='%s' AND passw='%s'" % (username, password)
+        sp_user_id = data_base_manager.exec_query(query)
+        if sp_user_id:
+            return redirect('connected/%s' % username)
+        else:
+            error = 'Invalid Credentials. Please try again.'
+    return render_template('Login.html', error=error)
 
 
 @app.route('/login/<username> <password>')
@@ -37,18 +49,26 @@ def private_login(username, password):
     #redirect to Service Provider
     #return redirect(('127.0.0.1', 80))
 
-def create_db():
-    fields = ["ID integer primary key autoincrement", "username text not null", "password text not null",
-              "spID int not null", "spUserID int not null" ]
-    data_base_manager.create_table("Users", fields)
+@app.route('/connected/<user>')
+def connected(user):
+    return 'Hello %s, you are successfully connected.' % user
 
-    fields = ["SPID integer primary key autoincrement", "details text not null", "key text not null" ]
+
+def create_db():
+    fields = ["ID integer primary key autoincrement", "username text not null", "passw text not null",
+              "spID int not null", "spUserID int not null"]
+    data_base_manager.create_table("Users", fields)
+    fields = [(1, 'elle', '12345', 1, 11), (2, 'elle1', '12345', 1, 11), (3, 'elle2', '12345', 1, 11)]
+    data_base_manager.insert("Users", fields)
+    data_base_manager.print_table("Users")
+
+    fields = ["SPID integer primary key autoincrement", "details text not null", "key text not null"]
     data_base_manager.create_table("SPs", fields)
 
 
 def main():
-    create_db()
-    app.run(host='127.0.0.1',port=8080)
+    #create_db()
+    app.run(host='127.0.0.1', port=80)
 
 if __name__ == '__main__':
     main()
